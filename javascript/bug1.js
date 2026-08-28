2b6e0e03765970d375c668c84684711c7fd777dd
Hemant <emeehemant159@gmail.com>
2026-08-28 11:40:56 +0530

    initial commit

---
+++ javascript/bug1.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  if (req.url = '/') {
+    res.statusCode = 200;
+    res.end('Hello World');
+  } else {
+    res.statusCode = 404;
+    res.end('Not Found');
+  }
+});
+
+server.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
