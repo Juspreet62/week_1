df7197f662dcd2f6d12515cb6eb1ec07f7a9ddf1
Hemant <emeehemant159@gmail.com>
2026-09-01 10:42:00 +0530

    added some more questions

---
+++ javascript/bug2.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+function requestHandler(req, res) {
+  if (req.url === '/') {
+    res.statusCode = 200;
+    console.log('Sending Hello World');
+  } else {
+    res.statusCode = 404;
+    res.end('Not Found');
+  }
+}
+
+const server = http.createServer(requestHandler);
+server.listen(3000, () => console.log('Server running on port 3000'));
---
+++ javascript/bug6.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+app.post('/users', (req, res) => {
+  const name = req.body.name;
+  res.send('Received: ' + name);
+});
+
+app.listen(3000, () => console.log('Server running on port 3000'));
